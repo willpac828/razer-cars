36e16da9693ecafea508fa46eed0f0dc71bdd7fa
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-03-21 21:43:36 -0500

    changed my name to ryan on api

--- app/manufacturers/route.js
+++ app/manufacturers/route.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const brands = 'https://razerapi.herokuapp.com/will/basic/brands';
+const brands = 'https://razerapi.herokuapp.com/ryan/basic/brands';
 
 export default Ember.Route.extend({
   model() {
